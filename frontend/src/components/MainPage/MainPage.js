2393e7aa3c179ac484c113276ac181157481f2fd
wc2184 <68214261+wc2184@users.noreply.github.com>
2022-10-26 09:20:21 -0400

    Routes and nav

---
+++ frontend/src/components/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+// src/components/MainPage/MainPage.js
+
+function MainPage() {
+  return (
+    <>
+      <p>A Twitter Clone</p>
+      <footer>Copyright &copy; 2022 Chirper</footer>
+    </>
+  );
+}
+
+export default MainPage;
